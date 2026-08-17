7debac12ce65376151d8fca27f18e137ac0cfb4b
Ismael Fernández <fernandez.ismael5>
2026-08-17 16:19:05 -0400

    Practicas 'Modularidad'

---
+++ principal.py
@@ -0,0 +1,7 @@
+from utilidades.calculadora import restar, sumar
+
+num1 = 15
+num2 = 5
+
+print(f"Suma: {sumar(num1, num2)}")
+print(f"Resta: {restar(num1,num2)}")
---
+++ utilidades/calculadora.py
@@ -0,0 +1,8 @@
+###..::Prueba de Importación ::.###
+
+
+def sumar(a , b):
+    return a + b 
+
+def restar(a , b):
+    return a - b
