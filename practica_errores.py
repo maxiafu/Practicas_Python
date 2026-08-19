87c2fffbb0af09b5157b1c62b302e579fd5570a7
Ismael Fernández <fernandez.ismael5>
2026-08-19 12:27:59 -0400

    Agregando la excepción ValueError | Continuo mi aprendizaje

--- practica_errores.py
+++ practica_errores.py
@@ -3,6 +3,8 @@ try:
     resultado = 10 / numero
 except ZeroDivisionError:
     print("Error: NO puedes dividir entre cero")
+except ValueError:
+    print("Error: Debes ingresar un número enero válido")
 else:
     print(f" Éxito: El resultado es {resultado}")
 finally:
