a27c48791358d446cdc6767fede40bd8e94dc76b
Ismael Fernández <fernandez.ismael5>
2026-08-20 15:42:07 -0400

    Update_Test

---
+++ pruebas -_asincronia ::..py
@@ -0,0 +1,21 @@
+###..:: Pruebas - Asincronia ::..####
+import asyncio
+
+
+async def preparar_cafe():
+    print("Iniciando Café")
+    await asyncio.sleep(3)# Libera el control mientras espera 3 segundos
+    print("Café Listo")
+
+
+async def tostada():
+    print("Haciendo Tostadas")
+    print("Tostada Lista")
+
+
+async def main():
+    # Ejecuta ambas tareas aprovechando el tiempo de espera
+    await asyncio.gather(preparar_cafe(), tostada())
+
+
+asyncio.run(main())
